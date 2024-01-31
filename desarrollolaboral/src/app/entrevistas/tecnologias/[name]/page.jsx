b07caab36130a83d8e3d1aeab28f90b5c39e4b0c
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-01-31 16:52:27 -0300

    Corregido volver

--- desarrollolaboral/src/app/entrevistas/tecnologias/[name]/page.jsx
+++ desarrollolaboral/src/app/entrevistas/tecnologias/[name]/page.jsx
@@ -16,7 +16,7 @@ export default async function Viewquestions({params}) {
       <div className="container px-4 md:px-6">
         <div className="text-center mb-8 flex justify-between">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-custom-green">Preguntas frecuentes sobre {params.name}</h2>
-          <Link href="http://localhost:3000/entrevistas/tecnologias"><button type="button" className="w-full flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+          <Link href="/entrevistas/tecnologias"><button type="button" className="w-full flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
     <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
     </svg>
